ccc9b07b31d4649cd1079d066c7b315283559108
Jm9710 <85179035+Jm9710@users.noreply.github.com>
2024-07-22 03:09:36 +0000

    generador de escusas terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return `${randomElement(who)} ${randomElement(action)} ${randomElement(
+    what
+  )} ${randomElement(when)}.`;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
